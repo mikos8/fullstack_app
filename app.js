2f36150cb7a40561a73513922f44a7e5820d5c4d
Miras Zhiyenbayev <65290147+mikos8@users.noreply.github.com>
2021-07-09 19:53:33 +0600

    add module

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
 const authRoutes = require('./routes/auth')
 const analiticsRoutes = require('./routes/analitics')
@@ -8,16 +8,26 @@ const positionRoutes = require('./routes/position')
 const bodyParser = require('body-parser')
 const cors = require('cors'); // мог обрабатывать корс 
 const morgan = require('morgan') // красиво логировать процесыы
+const mongoose = require('mongoose')
 
+
+mongoose.connect('srv://cluster0.t1ug3.mongodb.net/fullstack')
+    .then(() => {
+        console.log('MongoDB conected');
+    })
+    .catch(error=>console.log(error))
 app.use(morgan('dev'))
 app.use(cors())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 app.use(bodyParser.json())
 
-app.use('/api/auth',authRoutes)
-app.use('/api/analitics',analiticsRoutes)
-app.use('/api/category',categoryRoutes)
-app.use('/api/order',orderRoutes)
-app.use('/api/position',positionRoutes)
+app.use('/api/auth', authRoutes)
+app.use('/api/analitics', analiticsRoutes)
+app.use('/api/category', categoryRoutes)
+app.use('/api/order', orderRoutes)
+app.use('/api/position', positionRoutes)
+
 
 module.exports = app;
